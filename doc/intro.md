52ac4a66ce77bc075ca1919195e4d8b5c77c810c
Whit Chapman <whit.chapman@gmail.com>
2019-06-14 17:10:39 -0600

    Add lein template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-car-pooling
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_car_pooling/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-car-pooling.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-car-pooling.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
