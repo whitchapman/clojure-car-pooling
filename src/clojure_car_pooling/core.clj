0fb38900566616c44f69fc0eb1b9456c12c41b49
Whit Chapman <whit.chapman@gmail.com>
2019-06-19 08:28:28 -0600

    Add routes

--- src/clojure_car_pooling/core.clj
+++ src/clojure_car_pooling/core.clj
@@ -1,7 +1,77 @@
 (ns clojure-car-pooling.core
-  (:gen-class))
+  (:require [clojure.pprint :refer [pprint]]
+            [reitit.ring :as ring]
+            [reitit.coercion.spec]
+            [reitit.swagger :as swagger]
+            [reitit.swagger-ui :as swagger-ui]
+            [reitit.ring.coercion :as coercion]
+            [reitit.dev.pretty :as pretty]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.ring.middleware.exception :as exception]
+            [reitit.ring.middleware.multipart :as multipart]
+            [reitit.ring.middleware.parameters :as parameters]
+            [reitit.ring.middleware.dev :as dev]
+            [reitit.ring.spec :as spec]
+            [spec-tools.spell :as spell]
+            [ring.adapter.jetty :as jetty]
+            [muuntaja.core :as m]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/swagger.json"
+        {:get {:no-doc true
+               :swagger {:info {:title "Icelandic car pooling"
+                                :description "an extention of an existing API"}}
+               :handler (swagger/create-swagger-handler)}}]
+
+       ["/rides"
+        {:swagger {:tags ["rides"]}}
+
+        ["/samferda-drivers/"
+         {:get {:summary "gets list of drivers"
+                :handler (fn [_]
+                           {:status 200
+                            ;;:headers {"Content-Type" "application/json;charset=UTF-8"}
+                            :body {:message "list of drivers"}})}}]
+        ["/samferda-passengers/"
+         {:get {:summary "gets list of passengers"
+                :handler (fn [_]
+                           {:status 200
+                            ;;:headers {"Content-Type" "application/json;charset=UTF-8"}
+                            :body {:message "list of passengers"}})}}]]]
+
+      {;;:reitit.middleware/transform dev/print-request-diffs ;; pretty diffs
+       ;;:validate spec/validate ;; enable spec validation for route data
+       ;;:reitit.spec/wrap spell/closed ;; strict top-level validation
+       :exception pretty/exception
+       :data {:coercion reitit.coercion.spec/coercion
+              :muuntaja m/instance
+              :middleware [;; swagger feature
+                           swagger/swagger-feature
+                           ;; query-params & form-params
+                           parameters/parameters-middleware
+                           ;; content-negotiation
+                           muuntaja/format-negotiate-middleware
+                           ;; encoding response body
+                           muuntaja/format-response-middleware
+                           ;; exception handling
+                           exception/exception-middleware
+                           ;; decoding request body
+                           muuntaja/format-request-middleware
+                           ;; coercing response bodys
+                           coercion/coerce-response-middleware
+                           ;; coercing request parameters
+                           coercion/coerce-request-middleware
+                           ;; multipart
+                           multipart/multipart-middleware]}})
+    (ring/routes
+      (swagger-ui/create-swagger-ui-handler
+        {:path "/"
+         :config {:validatorUrl nil
+                  :operationsSorter "alpha"}})
+      (ring/create-default-handler))))
+
+(defn start []
+  (jetty/run-jetty app {:port 4040 :join? false})
+  (println "server running in port 4040"))
